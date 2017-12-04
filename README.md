c5cf9c84e41d757dc0848eca168657ff6d893a26
Samuel DIDIER-LAURENT <istuffs@gmail.com>
2017-12-04 14:33:15 +0100

    gulp starter kit

---
+++ README.md
@@ -0,0 +1,31 @@
+# template-gulp
+
+A gulp kick start template for fast and modern front-end development.
+
+All the gulp plugins:
+
+-   Gulp
+-   Autoprefixer
+-   Clean-css
+-   Htmlmin
+-   Plumber
+-   Sass
+-   Sourcemaps
+-   Uglify
+-   Plumber
+-   Notify
+-   Imagemin
+
+## What you need ?
+
+You fisrt need to have node installed.
+
+## How to use
+
+Customise the pakage.json infos.
+
+To install local dependencies you need to run
+`npm install`
+
+Then run gulp
+`gulp`
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+/* gulp plugins variables*/
+
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename       = require('gulp-rename'),
+    cleanCss     = require('gulp-clean-css'),
+    htmlMin      = require('gulp-htmlmin'),
+    uglify       = require('gulp-uglify'),
+    browserSync  = require('browser-sync'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    imagemin     = require('gulp-imagemin'),
+    babel        = require('gulp-babel'),
+    plumber      = require('gulp-plumber'),
+    notify       = require("gulp-notify");
+
+
+/* tasks declaration*/
+gulp.task('cssTask', function () {
+  return gulp.src('./src/sass/*.scss')
+  .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+      browsers: ['last 6 versions'],
+      cascade: false
+  }))
+  .pipe(cleanCss({
+    compatibility: 'ie8'
+  }))
+  .pipe(rename(function(path){ path.basename += ".min"; }))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('htmlTask', function() {
+  return gulp.src('src/*.html')
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(htmlMin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('jsTask', function() {
+  return gulp.src('./src/js/*.js')
+  .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+  .pipe(babel({ presets: ['es2015'] }))
+  .pipe(uglify())
+  .pipe(rename(function(path){ path.basename += ".min"; }))
+  .pipe(gulp.dest('./dist/js'));
+});
+gulp.task('compression', function() {
+      return gulp.src('src/img/*.{gif,jpg,png,svg,jpeg}')
+      .pipe(imagemin())
+      .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('refresh', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist/"
+    }
+  });
+});
+
+/* default task and watch */
+gulp.task('watch', ['cssTask', 'jsTask', 'htmlTask', 'refresh', 'compression'], function () {
+  gulp.watch('./src/sass/*/*.scss', ['cssTask']);
+  gulp.watch('./src/js/*.js', ['jsTask']);
+  gulp.watch('./src/*.html', ['htmlTask']);
+  gulp.watch('./dist/*.html').on('change', browserSync.reload);
+  gulp.watch('./dist/css/*.css').on('change', browserSync.reload);
+  gulp.watch('./dist/js/*.js').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
