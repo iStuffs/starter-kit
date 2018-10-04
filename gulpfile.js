9fb7ec8f5f6a9c99265d2fcacaab4ae5ee4c70a0
Samuel <samuel.didier-laurent@idweaver.com>
2018-10-04 16:52:29 +0200

    build(gulp): upgrade to gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -20,8 +20,8 @@ const { argv }     = require('yargs');
 
 
 /* tasks declaration */
-gulp.task('cssTask', () => {
-    gulp.src('./src/sass/**/*.{sass,scss}')
+function cssTask() {
+    return gulp.src('./src/sass/**/*.{sass,scss}')
         .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
         .pipe(gulpif(!argv.production, sourcemaps.init()))
         .pipe(sass().on('error', sass.logError))
@@ -35,57 +35,58 @@ gulp.task('cssTask', () => {
         .pipe(rename((path) => { path.basename += '.min'; }))
         .pipe(gulpif(!argv.production, sourcemaps.write('.')))
         .pipe(gulp.dest('./dist/css'));
-});
+}
 
-gulp.task('htmlTask', () => {
-    gulp.src('src/*.html')
+function htmlTask() {
+    return gulp.src('src/*.html')
         .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
         .pipe(gulpif(argv.production, htmlMin({ collapseWhitespace: true })))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('jsTask', () => {
-    gulp.src('./src/js/*.js')
+function jsTask() {
+    return gulp.src('./src/js/*.js')
         .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
         .pipe(babel({ presets: ['env'] }))
         .pipe(gulpif(argv.production, uglify()))
         .pipe(rename((path) => { path.basename += '.min'; }))
         .pipe(gulp.dest('./dist/js'));
-});
+}
 
-gulp.task('imgTask', () => {
-    gulp.src('src/img/*.{gif,jpg,png,svg,jpeg}')
+function imgTask() {
+    return gulp.src('src/img/*.{gif,jpg,png,svg,jpeg}')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
-});
+}
 
-gulp.task('compress', () => {
-    gulp.src('dist/**/*')
+function compress() {
+    return gulp.src('dist/**/*')
         .pipe(zip(`${process.env.npm_package_name}.zip`))
         .pipe(gulp.dest('./'));
-});
+}
 
-gulp.task('cleanDist', (done) => {
+function cleanDist(done) {
     rm('./dist/', done);
-});
+}
 
-gulp.task('refresh', () => {
+function refresh(done) {
     browserSync.init({
         server: {
             baseDir: './dist/',
         },
         port: '8080',
     });
-});
+    done();
+}
 
 /* default task and watch */
-gulp.task('watch', ['cssTask', 'jsTask', 'htmlTask', 'refresh', 'imgTask'], () => {
-    gulp.watch('./src/sass/**/*.{sass,scss}', ['cssTask']);
-    gulp.watch('./src/js/*.js', ['jsTask']);
-    gulp.watch('./src/*.html', ['htmlTask']);
+gulp.task('watch', gulp.series(cssTask, jsTask, htmlTask, refresh, imgTask, () => {
+    gulp.watch('./src/sass/**/*.{sass,scss}', gulp.series(cssTask));
+    gulp.watch('./src/js/*.js', gulp.series(jsTask));
+    gulp.watch('./src/*.html', gulp.series(htmlTask));
     gulp.watch('./dist/*.html').on('change', browserSync.reload);
     gulp.watch('./dist/css/*.css').on('change', browserSync.reload);
     gulp.watch('./dist/js/*.js').on('change', browserSync.reload);
-});
+}));
 
-gulp.task('default', ['watch']);
+gulp.task('default', gulp.series('watch'));
