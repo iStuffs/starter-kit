ee009130342c81f502535e21e8ff132163addda7
Samuel DIDIER-LAURENT <istuffs@gmail.com>
2019-03-29 17:32:36 +0100

    futhark reboot (wip)

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
-# Front-end **Starter Kit**
-![](docs/img/starter-kit_header.png)
-A npm / gulp kick start template for fast and modern front-end development.
+# Futark **:style**
+![](docs/img/futhark-logo.png)
 
 **Featues**:
 
--   Sass compilation with [gulp-sass](https://www.npmjs.com/package/gulp-sass)
--   Es6 transpilation with [babel-loader](https://www.npmjs.com/package/babel-loader)
--   JavaScript exports with [webpack](https://webpack.js.org/)
--   Auto-refresh browser with [browser sync](https://www.npmjs.com/package/browser-sync)
--   [Source maps](https://www.npmjs.com/package/gulp-sourcemaps) in dev mode
--   Minification in production ([Clean CSS](https://www.npmjs.com/package/gulp-clean-css) and [Uglify](https://www.npmjs.com/package/gulp-uglify))
--   Autoprefix CSS with [Autoprefixer](https://www.npmjs.com/package/gulp-autoprefixer)
--   Better errors message in gulp with [Plumber](https://www.npmjs.com/package/gulp-plumber) and [Notify](https://www.npmjs.com/package/gulp-notify)
--   Compress images with [Image min](https://www.npmjs.com/package/gulp-imagemin)
+-   Sass map for variables
+-   Color System
+-   Animation defaults
+-   Function utilities
+-   Usefull mixins: (aspect-ratio, hover, triangle, text-ellipis)
+-   Includes reset (boostrap-reboot, normalize-css)
+-   Easy import with eyeglass
 
 **The SASS folder structure**:
 
@@ -37,42 +34,19 @@ sass/
 
 Sass Documentation : https://istuffs.github.io/starter-kit/
 
-## What you need ?
+## Installation
 
-You first need to have [node.js](https://nodejs.org/en/) installed.
-You should use [git](https://git-scm.com/) with your project.
+```shell
+npm i -S @futark:style
+```
 
 ## How to use
 
-1. Clone it
-```bash
-git clone https://github.com/iStuffs/starter-kit.git my-new-project
-```
+Import in your build tool or Bundler
 
-2. Update your github user in the package.json
-```json
-...
-"gitUser": "Your-GitHubUserName",
-...
-```
+1. Webpack
 
-3. kickstart your project
-```bash
-cd my-new-project
-npm run starter
-```
 
-4. Develop awesome things
-```bash
-npm start
-```
+2. Gulp (with eyeglass)
 
-5. Build assets for production
-```bash
-npm run build
-```
-
-6. Deploy on git hub pages
-```bash
-npm run deploy
-```
+3.  Gulp (with include-path)
--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,26 @@
 /* Imports */
 const args = require('yargs').argv;
-const browserSync = require('browser-sync');
 const eyeglass = require('eyeglass');
 const gulp = require('gulp');
-const named = require('vinyl-named');
-const panini = require('panini');
 const plugins = require('gulp-load-plugins');
 const rm = require('rimraf');
 const sassdoc = require('sassdoc');
-const webpack = require('webpack');
-const webpackStream = require('webpack-stream');
 
 /* Plugins */
-// { autoprefixer, cleanCss, htmlmin, if, imagemin, notify, plumber, sass, sassGlob, sourcemaps, uglify, zip }
+// { autoprefixer, cleanCss, if, notify, plumber, sass, sassGlob, sourcemaps, uglify, zip }
 const $ = plugins();
 
 /* Configuration */
 const {
-    ASSETS,
     COMPATIBILITY,
     CSS,
     DOC,
     ERROR,
-    HTML,
-    IMAGES,
-    JS,
     PATH,
-    SERVER,
 } = require('./config.json');
 
 const production = !!args.production;
 
-/* ASSETS - json and fonts */
-function assets() {
-    return gulp
-        .src(PATH.src + ASSETS.src)
-        .pipe(gulp.dest(PATH.dest + ASSETS.dest)); // JSON (.json) and fonts (*.{eot,otf,svg,ttf,woff,woff2})
-}
 
 /* CSS */
 function css() {
@@ -61,77 +45,6 @@ function css() {
         .pipe(gulp.dest(PATH.dest + CSS.dest));
 }
 
-/* HTML */
-function html() {
-    return gulp
-        .src(PATH.src + HTML.entries)
-        .pipe($.plumber({ errorHandler: $.notify.onError(ERROR) }))
-        .pipe(panini(HTML.paniniOptions))
-        .pipe($.if(production, $.htmlmin({ collapseWhitespace: true })))
-        .pipe(gulp.dest(PATH.dest + HTML.dest));
-}
-
-function paniniRefresh(done) {
-    panini.refresh();
-    done();
-}
-
-/* IMAGES */
-function images() {
-    return gulp
-        .src(PATH.src + IMAGES.src)
-        .pipe($.imagemin())
-        .pipe(gulp.dest(PATH.dest + IMAGES.dest));
-}
-
-/* JS */
-const webpackConfig = {
-    mode: production ? 'production' : 'development',
-    module: {
-        rules: [
-            {
-                test: /.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env'],
-                            compact: false,
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    devtool: !production && 'source-map',
-};
-
-function js() {
-    return gulp
-        .src(PATH.src + JS.entries)
-        .pipe(named())
-        .pipe($.plumber({ errorHandler: $.notify.onError(ERROR) }))
-        .pipe($.if(!production, $.sourcemaps.init()))
-        .pipe(webpackStream(webpackConfig, webpack))
-        .pipe($.if(production, $.uglify()))
-        .pipe($.if(!production, $.sourcemaps.write('.')))
-        .pipe(gulp.dest(PATH.dest + JS.dest));
-}
-
-/* OTHERS FUNCTIONS */
-// Archive
-function archive() {
-    return gulp
-        .src('dist/**/*')
-        .pipe($.zip(`${process.env.npm_package_name}.zip`))
-        .pipe(gulp.dest('./'));
-}
-
-// Browser Reload
-function browserReload(done) {
-    browserSync.reload();
-    done();
-}
 
 // Clean - Clean destination directory
 function clean(done) {
@@ -143,55 +56,19 @@ function doc() {
     return gulp.src('./src/sass/**/*.scss').pipe(sassdoc(DOC.sassDocOptions));
 }
 
-/* Archive */
-gulp.task('archive', gulp.series(archive));
 
 /* Build */
-gulp.task('build', gulp.series(clean, assets, css, js, images, html));
+gulp.task('build', gulp.series(clean, css));
 
 /* Doc */
 gulp.task('doc', gulp.series(doc));
 
-/* Serve */
-gulp.task('serve', (done) => {
-    browserSync.init({
-        server: {
-            baseDir: PATH.dest,
-        },
-        port: SERVER.port,
-    });
-    done();
-});
-
 /* Watching */
 gulp.task(
     'watch',
-    gulp.series('build', 'serve', () => {
-        // assets
-        gulp.watch(PATH.src + ASSETS.src, gulp.series(assets)).on(
-            'all',
-            gulp.series(browserReload),
-        );
+    gulp.series('build', () => {
         // css
-        gulp.watch(PATH.src + CSS.src, gulp.series(css)).on(
-            'all',
-            gulp.series(browserReload),
-        );
-        // html
-        gulp.watch(PATH.src + HTML.src).on(
-            'all',
-            gulp.series(paniniRefresh, html, browserReload),
-        );
-        // images
-        gulp.watch(PATH.src + IMAGES.src, gulp.series(images)).on(
-            'all',
-            gulp.series(browserReload),
-        );
-        // javascript
-        gulp.watch(PATH.src + JS.src, gulp.series(js)).on(
-            'all',
-            gulp.series(browserReload),
-        );
+        gulp.watch(PATH.src + CSS.src, gulp.series(css));
     }),
 );
 
